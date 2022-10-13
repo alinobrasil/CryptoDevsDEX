d6ff3af4633cea6aaa9b262430f6097a00b51ead
Ali Kim <alikim@gmail.com>
2022-10-13 00:41:00 -0400

    first commit

---
+++ frontend/constants/index.js
@@ -0,0 +1,4 @@
+export const TOKEN_CONTRACT_ABI = require("./CryptoDevToken.json").abi;
+export const TOKEN_CONTRACT_ADDRESS = "0x79397C5B252D862362e84f1e1aDC229ce85C55ff";
+export const EXCHANGE_CONTRACT_ABI = require('./Exchange.json').abi
+export const EXCHANGE_CONTRACT_ADDRESS = "0x726Cabb5e767c547cc734F744b9B0F9C037Bf58c";
---
+++ hardhat/constants/index.js
@@ -0,0 +1,7 @@
+// Replace the value with your NFT contract address
+const CRYPTO_DEV_TOKEN_CONTRACT_ADDRESS = "0x79397C5B252D862362e84f1e1aDC229ce85C55ff";
+
+module.exports = { CRYPTO_DEV_TOKEN_CONTRACT_ADDRESS };
+
+
+// Exchange Contract Address: 0x726Cabb5e767c547cc734F744b9B0F9C037Bf58c
